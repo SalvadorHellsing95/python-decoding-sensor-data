39fe454682990369c8d5a1ae1c83c9e3f72a492c
Jose Salvador Gonzalez Blanco <jose.blanco@ugto.mx>
2022-01-06 13:30:50 -0600

    get particle data by area with sensor_app

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -1,5 +1,7 @@
 from house_info import HouseInfo
 from datetime import date, datetime
+
+
 class ParticleData(HouseInfo):
     def _convert_data(self, data):
         recs = []
@@ -10,6 +12,23 @@ class ParticleData(HouseInfo):
     def get_data_by_area(self, rec_area=0):
         recs = super().get_data_by_area('particulate', rec_area)
         return self._convert_data(recs)
+
     def get_data_by_date(self, rec_date=date.today()):
         recs = super().get_data_by_date('particulate', rec_date)
         return self._convert_data(recs)
+
+    def get_data_concentrations(self, data):
+        # particulate = dict(good=0, moderate=0, bad=0) unittest not working with this :c
+        particulate = {'good': 0, 'moderate': 0, 'bad': 0}
+        for rec in data:
+            rec = float(rec)
+            if rec <= 50.0 :
+                particulate['good'] += 1
+            elif rec <= 100.0:
+                particulate['moderate'] += 1
+            else:
+                particulate['bad'] += 1
+
+        return particulate
+
+
